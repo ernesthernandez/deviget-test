6c04f09dbf1c318c6526074e87a379a80f95c0b3
Ernest Hernandez <htc.ernest.hernandez@gmail.com>
2023-05-17 19:49:13 -0500

    DEVI-05: Implement mixin

--- app/design/frontend/Ernest/ww_default/Magento_Swatches/web/js/swatch-renderer-mixin.js
+++ app/design/frontend/Ernest/ww_default/Magento_Swatches/web/js/swatch-renderer-mixin.js
@@ -1,12 +1,32 @@
 define([
-    'jquery'
-], function ($) {
+    'jquery',
+    'mage/translate',
+    'Magento_Ui/js/modal/alert'
+], function ($, $t, alert) {
     'use strict';
 
     return function (widget) {
         $.widget('mage.SwatchRenderer', widget, {
-            _Rebuild: function () {
-                return this._super();
+            /**
+             * Logic to add tooltip into the swatches
+             * @returns {*}
+             * @private
+             */
+            _OnClick: function ($this, $widget) {
+                let _self = this;
+                alert({
+                    title: $t('Low Stock of this product hurry up!'),
+                    content: $('.alert-modal-content'),
+                    modalClass: 'alert',
+                    buttons: [{
+                        text: $.mage.__('Select'),
+                        class: 'action primary accept',
+                        click: function () {
+                            this.closeModal(true);
+                        }
+                    }]
+                });
+                _self._super($this, $widget)
             }
         });
 
